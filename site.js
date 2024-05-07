e0f5f6978a6610de479f481b45c4306be1db40ec
MReiss97 <maxreiss21@gmail.com>
2024-05-07 13:06:50 -0400

    Step 4 finished; added title, name, and link

--- site.js
+++ site.js
@@ -34,7 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
+            title: "IMDB + Max's Top 8 Movies",
+            owner: 'Max',
+            github: 'https://github.com/MReiss97/NJIT-3_StarterFiles', 
       }
     },
       methods: {
